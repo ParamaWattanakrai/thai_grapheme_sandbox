762c36d987ae97ecc2094a288f720a4dff728a0f
Parama Wattanakrai <Parama.Wattanakrai@gmail.com>
2026-07-03 22:55:36 +0700

    test

--- test1.py
+++ test1.py
@@ -1,8 +1,9 @@
 import thai_syl
 
-old_thai = thai_syl.extract('หา', force_cluster=False, sesquisyllable=False)
+old_thai = thai_syl.extract('ฃวา', force_cluster=False, sesquisyllable=False)
 print(old_thai)
 standard_thai = thai_syl.sound_shift(old_thai, thai_syl.STANDARD_THAI_SOUND_SHIFTS)
 print(standard_thai)
 
+print(thai_syl.get_ipa(old_thai, reduplicate=True))
 print(thai_syl.get_ipa(standard_thai, reduplicate=True))
