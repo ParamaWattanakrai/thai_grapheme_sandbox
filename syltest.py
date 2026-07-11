8c0f26da0cbfa65b06b37a1c058abf107be35b83
Parama Wattanakrai <Parama.Wattanakrai@gmail.com>
2026-07-12 03:57:44 +0700

    idk

--- syltest.py
+++ syltest.py
@@ -7,6 +7,8 @@ syllable2.assimilate_tone(syllable1)
 syllable1.sound_shift()
 syllable2.sound_shift()
 
-print(syllable1.get_ipa() + syllable2.get_ipa())
+print(syllable1, syllable2)
 print(syllable1.reconstruct_text() + syllable2.reconstruct_text())
 print(f'{syllable1.main_syllable.tone_split} - {syllable2.main_syllable.assimilated_tone_split}({syllable2.main_syllable.tone_split})')
+
+print(Syllable.extract('กกิ', force_cluster=False, sesquisyllable=True))
